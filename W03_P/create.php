66ff6b399a0cc96da2f0043ecabd238038bf322c
rladmsdud1em <70553171+rladmsdud1em@users.noreply.github.com>
2020-09-21 03:51:10 +0900

    Add files via upload

---
+++ W03_P/create.php
@@ -0,0 +1,40 @@
+<?php
+  $link = mysqli_connect('localhost','root','k5669203','tra');
+  $query = "SELECT * FROM topic";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while($row = mysqli_fetch_array($result)){
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+  $article = array(
+    'title' => 'Welcome',
+    'description' => '신종 코로나 바이러스로 여름 휴가철 내내 햇빛을 보지 못한 나에게 주는 언택트 여행.'
+  );
+
+  if(isset($_GET['id']) ) {
+    $query = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+  }
+ ?>
+
+<!DOCTYPE html>
+<html style="text-align:center">
+<head>
+  <meta charset="utf-8">
+  <title style="color:green"> ♣Untact travel♣ </title>
+</head>
+<body>
+  <h1><a href="index.php" style="color:green">♣Untact travel♣</a></h1>
+  <ol> <?= $list ?> </ol>
+  <form action="process_create.php" method="POST">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p><textarea name="description" placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+  </form>
+</body>
+</html>
---
+++ W03_P/index.php
@@ -0,0 +1,50 @@
+<?php
+  $link = mysqli_connect('localhost','root','k5669203','tra');
+  $query = "SELECT * FROM topic";
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while($row = mysqli_fetch_array($result)){
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+  $article = array(
+    'title' => 'Welcome',
+    'description' => '신종 코로나 바이러스로 여름 휴가철 내내 햇빛을 보지 못한 나에게 주는 언택트 여행.'
+  );
+
+  $update_link='';
+  $delete_link='';
+
+  if(isset($_GET['id']) ) {
+    $query = "SELECT * FROM topic WHERE id={$_GET['id']}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+    $update_link='<a href="update.php?id='.$_GET['id'].'">update</a>';
+    $delete_link='
+      <form action="process_delete.php" method="POST">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type="submit" value="delete">
+      </form>
+      ';
+  }
+ ?>
+
+<!DOCTYPE html>
+<html style="text-align:center">
+<head>
+  <meta charset="utf-8">
+  <title> ♣Untact travel♣ </title>
+</head>
+<body>
+  <h1><a href="index.php" style="color:green">♣Untact travel♣</a></h1>
+  <ol> <?= $list ?> </ol>
+  <a href="create.php" style="color:pink">create</a>
+  <?=$update_link?>
+  <?=$delete_link?>
+  <h2> <?= $article['title']?> </h2>
+   <?= $article['description'] ?>
+</body>
+</html>
